56f839b9a63b31d539946fbbe9e09bf2ce2cd337
Beldur <alexluecking@gmail.com>
2013-02-16 02:44:47 +0100

    Added DownloadOptions struct

---
+++ src/ytlib/ytlib.go
@@ -0,0 +1,9 @@
+package ytlib
+
+// YouTube Video url
+const YT_URL = "http://www.youtube.com/get_video_info?hl=en_US&el=detailpage&video_id="
+
+type DownloadOptions struct {
+    Format int
+    Begin int
+}
